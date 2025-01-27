cf7d64185d70f3fc66a23a9b8ca4d43181cfbed9
Ramanuj Dad <rampooja245@gmail.com>
2025-01-27 18:52:15 +0530

    loop range based on array

--- examples/07_loops/main.go
+++ examples/07_loops/main.go
@@ -5,10 +5,18 @@ import "fmt"
 func main() {
 	println("Starting Loops Example")
 	classicLoop()
+	rangeKeyLoop()
 }
 
 func classicLoop() {
 	for x := 0; x < 10; x++ {
 		fmt.Println(x)
+	}
+}
+func rangeKeyLoop() {
+	// create array
+	var fruits = []string{"Apple", "Orange", "Banana", "Grape"}
+	for index, fruit := range fruits {
+		fmt.Printf("Index: %d, Fruit: %s\n", index, fruit)
 	}
 }
